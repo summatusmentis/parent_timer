ebebc310db10396a4be7afd09703e2959e90b85c
summatusmentis <summatusmentis@users.noreply.github.com>
2022-03-19 11:30:11 -0500

    see if this works

---
+++ timer.js
@@ -0,0 +1,32 @@
+function startParentTimer() {
+    parent_time = document.getElementById("parentTimer").value
+    kid_time = document.getElementByid("kidTime").value
+    // if both were set to 60s, parent_time / kid_time would be 1000 ms
+    // if parent_time > kid_time, the update rate is > 1000 ms
+    // if parent_time < kid_time, the update rate is < 1000 ms
+    update_rate = int(int((int(parent_time) * 60) / (int(kid_time) * 60)) / 1000)
+    timerInterval = setInterval(()=> {updateTimer()},update_rate)
+    
+}
+
+function resetParentTimer() {
+    clearInterval(timerInterval)
+    document.getElementById("timer") = "0:00"
+
+}
+
+function updateTimer() {
+    timerString = document.getElementById("timer").value
+    timerArr = timerString.split(":")
+    minutes = int(timerArr[0])
+    seconds = int(timerArr[1])
+    
+    if(seconds === 0) {
+        minutes--;
+        seconds=59;
+    } else {
+        seconds--;
+    }
+    
+    document.getElementById("timer") = minutes + ":" + seconds;
+}
